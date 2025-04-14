65c1a461e75e5675162ab06d7f9ff739de7e5dec
Mingi Lee <mk8048@naver.com>
2025-04-14 22:54:56 +0900

    빼기 만들었습니다.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnsub;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("계산기");
+
+        Log.d("MainActivity", "onCreate called"); // 추가!
+
+        btnsub = (Button) findViewById(R.id.btnSub);
+        if (btnsub == null) {
+            Log.e("MainActivity", "btnSub is NULL!"); // 추가!
+        } else {
+            Log.d("MainActivity", "btnSub found"); // 추가!
+        }
+
+        btnsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("MainActivity", "btnSub clicked"); // 클릭 확인용
+                Intent intent = new Intent(MainActivity.this, Subtraction.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/calculator/Subtraction.java
@@ -0,0 +1,55 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Subtraction extends AppCompatActivity {
+
+    Button btnsub;
+    Button btnback;
+    EditText edit1;
+    EditText edit2;
+    TextView textresult;
+    String num1;
+    String num2;
+    int result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subtraction);
+        setTitle("빼기");
+
+        btnsub = (Button) findViewById(R.id.btnSub);
+        btnback = (Button) findViewById(R.id.btnBack);
+        edit1 = (EditText) findViewById(R.id.num1);
+        edit2 = (EditText) findViewById(R.id.num2);
+        textresult = (TextView)findViewById(R.id.textResult);
+
+        btnsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) - Integer.parseInt(num2);
+                textresult.setText("결과 : " + result);
+            }
+        });
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Subtraction", "btnSub clicked"); // 클릭 확인용
+                Intent backintent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(backintent);
+            }
+        });
+    }
+}
