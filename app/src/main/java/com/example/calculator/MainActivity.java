d40992b188d6f94b2e66824f9a56f4b03d4c2c32
Mingi Lee <mk8048@naver.com>
2025-04-16 22:49:32 +0900

    합칩니다.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -17,23 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("계산기");
 
-        Log.d("MainActivity", "onCreate called"); // 추가!
-
-        btnsub = (Button) findViewById(R.id.btnSub);
-        if (btnsub == null) {
-            Log.e("MainActivity", "btnSub is NULL!"); // 추가!
-        } else {
-            Log.d("MainActivity", "btnSub found"); // 추가!
-        }
-
         btnsub.setOnClickListener(new View.OnClickListener() { //빼기 액티비티로 GO
             @Override
             public void onClick(View view) {
-                Log.d("MainActivity", "btnSub clicked"); // 클릭 확인용
                 Intent intent = new Intent(MainActivity.this, Subtraction.class);
                 startActivity(intent);
             }
         });
+
+        //더하기
+
+        //나누기
+
+        //곱셈
     }
 
 }
